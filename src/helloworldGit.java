3c39a7ed98a756a0a0866e6f0d80b837780c8a6f
trungnqhust <ahnghuenqt@gmail.com>
2016-11-04 11:03:20 +0700

    commit 2 change second time

--- src/helloworldGit.java
+++ src/helloworldGit.java
@@ -5,6 +5,6 @@ public class helloworldGit {
     public static void main(String[] args) {
         System.out.println("Hello Demo Git ");
         System.out.println("change a little bit here");
-        
+        System.out.println("change a little bit second time here");
     }
 }
